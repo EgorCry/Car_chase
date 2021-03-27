6ab56f008d24c926d1ae351a8a13d34ef30fd120
Wanderer <abulapin@mail.ru>
2021-03-27 23:00:13 +0300

    Added class Player, updated game loop

--- main.py
+++ main.py
@@ -40,6 +40,32 @@ class Rival(pygame.sprite.Sprite):
         surface.blit(self.image, self.rect)
 
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load('Sprites/Player.png')
+        self.surf = pygame.Surface((50, 100))
+        self.rect = self.surf.get_rect()
+
+    def update(self):
+        pressed_keys = pygame.key.get_pressed()
+        # if pressed_keys[K_UP]:
+            # self.rect.move_ip(0, -5)
+        # if pressed_keys[K_DOWN]:
+            # self.rect.move_ip(0, 5)
+        if self.rect.left > 0:
+            if pressed_keys[K_LEFT]:
+                self.rect.move_ip(-5, 0)
+        if self.rect.right < 400:
+            if pressed_keys[K_RIGHT]:
+                self.rect.move_ip(5, 0)
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+P1 = Player()
+E1 = Rival()
+
 # Set game loop
 while True:
     pygame.display.update()
@@ -47,5 +73,12 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+    P1.update()
+    E1.move()
 
+    DISPLAYSURF.fill(WHITE)
+    P1.draw(DISPLAYSURF)
+    E1.draw(DISPLAYSURF)
+
+    pygame.display.update()
     FramePerSec.tick(FPS)
