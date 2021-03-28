699f2356fad4bb7cf1cf666b332b23a14279f661
Wanderer <abulapin@mail.ru>
2021-03-28 20:58:42 +0300

    Added background, scores, crash sound

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import random
+import time
 from pygame.locals import *
 
 # Initialize the program
@@ -17,8 +18,24 @@ BLUE = (0, 0, 255)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-# Setup 400x600 pixels display with caption
-DISPLAYSURF = pygame.display.set_mode((400, 600))
+
+# Screen variables
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
+SPEED = 5
+SCORE = 0
+
+
+# Setting up fonts
+font = pygame.font.SysFont('Verdana', 60)
+font_small = pygame.font.SysFont('Verdana', 20)
+game_over = font.render('Game Over', True, BLACK)
+
+background = pygame.image.load('Sprites/AnimatedStreet.png')
+
+
+# Setup 400x600 pixels display with caption and white screen
+DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption('Car chase')
 
@@ -27,58 +44,90 @@ class Rival(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load('Sprites/Enemy.png')
-        self.surf = pygame.Surface((50, 80))
-        self.rect = self.surf.get_rect(center=(random.randint(40, 360), 0))
+        self.surf = pygame.Surface((42, 70))
+        self.rect = self.surf.get_rect(center=(random.randint(40, SCREEN_WIDTH-40), 0))
 
     def move(self):
-        self.rect.move_ip(0, 10)
+        global SCORE
+        self.rect.move_ip(0, SPEED)
         if self.rect.bottom > 600:
+            SCORE += 1
             self.rect.top = 0
-            self.rect.center = (random.randint(30, 370), 0)
-
-    def draw(self, surface):
-        surface.blit(self.image, self.rect)
+            self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load('Sprites/Player.png')
-        self.surf = pygame.Surface((50, 100))
-        self.rect = self.surf.get_rect()
+        self.surf = pygame.Surface((40, 75))
+        self.rect = self.surf.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT - 70))
 
-    def update(self):
+    def move(self):
         pressed_keys = pygame.key.get_pressed()
         # if pressed_keys[K_UP]:
-            # self.rect.move_ip(0, -5)
+        # self.rect.move_ip(0, -5)
         # if pressed_keys[K_DOWN]:
-            # self.rect.move_ip(0, 5)
+        # self.rect.move_ip(0, 5)
         if self.rect.left > 0:
             if pressed_keys[K_LEFT]:
                 self.rect.move_ip(-5, 0)
-        if self.rect.right < 400:
+        if self.rect.right < SCREEN_WIDTH-5:
             if pressed_keys[K_RIGHT]:
                 self.rect.move_ip(5, 0)
 
-    def draw(self, surface):
-        surface.blit(self.image, self.rect)
 
+# Set up sprites
 P1 = Player()
 E1 = Rival()
 
+
+# Set up sprites groups
+enemies = pygame.sprite.Group()
+enemies.add(E1)
+all_sprites = pygame.sprite.Group()
+all_sprites.add(P1)
+all_sprites.add(E1)
+
+
+# Adding a new User event
+INC_SPEED = pygame.USEREVENT + 1
+pygame.time.set_timer(INC_SPEED, 1000)
+
+
 # Set game loop
 while True:
-    pygame.display.update()
+    # Cycles through all events occurring
     for event in pygame.event.get():
+        if event.type == INC_SPEED:
+            SPEED += 0.5
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-    P1.update()
-    E1.move()
 
-    DISPLAYSURF.fill(WHITE)
-    P1.draw(DISPLAYSURF)
-    E1.draw(DISPLAYSURF)
+    DISPLAYSURF.blit(background, (0, 0))
+    scores = font_small.render(str(SCORE), True, BLACK)
+    DISPLAYSURF.blit(scores, (10, 10))
+
+    # Moves and Re-draw all Sprites
+    for entity in all_sprites:
+        DISPLAYSURF.blit(entity.image,entity.rect)
+        entity.move()
+
+    # To be run if collusion occurs between Player and Rival
+    if pygame.sprite.spritecollideany(P1, enemies):
+        pygame.mixer.Sound('Sounds/crash.wav').play()
+        time.sleep(0.5)
+
+        DISPLAYSURF.fill(RED)
+        DISPLAYSURF.blit(game_over, (30, 250))
+
+        pygame.display.update()
+        for entity in all_sprites:
+            entity.kill()
+        time.sleep(2)
+        pygame.quit()
+        sys.exit()
 
     pygame.display.update()
     FramePerSec.tick(FPS)
